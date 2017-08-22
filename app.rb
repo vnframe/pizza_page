1326c0de7e04d83fe1b243b2c6e403c78e8fd7a7
vnframe <vnframe95@icloud.com>
2017-08-22 15:46:22 -0400

    added some more toppings, started confirm page

--- app.rb
+++ app.rb
@@ -5,9 +5,17 @@ get "/" do
 end
 post "/index" do
     session[:sauce_type] = params[:sauce]
+    session[:cheese_type] = params[:cheese]
+    session[:meat_type] = params[:meat]
+    session[:veg_type] = params[:veg]
+    session[:pizza_size] = params[:size]
+    #puts "in post index, pizza size is #{pizza_size}"
+    #  puts "in post index, sauce is #{sauce_type}"
+
     redirect "/confirm"
     "#{sauce_type} is the sauce"
 end
 get "/confirm" do 
-    erb :confirm, locals: {sauce: session[:sauce_type]}
+    # puts "in get confirm, pizza size is #{pizza_size}"
+    erb :confirm, locals: {sauce: session[:sauce_type], cheese: session[:cheese_type], meat: session[:meat_type], veg: session[:veg_type], size: session[:pizza_size]}
 end
