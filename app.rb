3c5560bf90306dd10bdc014b53df9819deec033d
vnframe <vnframe95@icloud.com>
2017-08-23 17:00:49 -0400

    got all toppings showing on the last page in a hash

--- app.rb
+++ app.rb
@@ -19,15 +19,17 @@ get "/confirm" do
     erb :confirm, locals: {sauce: session[:sauce_type], cheese: session[:cheese_type], meat: session[:meat_type], veg: session[:veg_type], size: session[:pizza_size]}
 end
 post '/confirm' do
-    session[:sauce_type] = params[:sauce]
-    session[:cheese_type] = params[:cheese]
-    session[:meat_type] = params[:meat]
-    session[:veg_type] = params[:veg]
-    session[:pizza_size] = params[:size]
+    session[:sauce_type] = params[:sauces]
+    session[:cheese_type] = params[:cheeses]
+    session[:meat_type] = params[:meats]
+    session[:veg_type] = params[:vegs]
+    session[:pizza_size] = params[:sizes]
+    p "sauce is #{session[:sauce_type]}"
+    p "cheese is #{session[:cheese_type]}"
     redirect "/complete"
   end
-  get "/complete" do
-   erb :complete, locals: {sauce: session[:sauce_type], cheese: session[:cheese_type], meat: session[:meat_type], veg: session[:veg_type], size: session[:pizza_size]}
+  get '/complete' do
+   erb :complete, locals: {sauce_final: session[:sauce_type], cheese_final: session[:cheese_type], meat_final: session[:meat_type], veg_final: session[:veg_type], size_final: session[:pizza_size]}
   end
 #   post "/complete" do
 #     session[:sauce_type] = params[:sauce]
