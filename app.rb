d41814bf043a73b271d0bcb7b22bc01006df9416
vnframe <vnframe95@icloud.com>
2017-08-24 22:05:25 -0400

    prompts for address and carries to delivery page, but no still doesn't work

--- app.rb
+++ app.rb
@@ -24,18 +24,25 @@ post '/confirm' do
     session[:meat_type] = params[:meats]
     session[:veg_type] = params[:vegs]
     session[:pizza_size] = params[:size]
+    session[:deliver] = params[:delivery]
     p "sauce is #{session[:sauce_type]}"
     p "cheese is #{session[:cheese_type]}"
     p "veg is #{}"
     redirect "/complete"
   end
   get '/complete' do
-   erb :complete, locals: {sauce_final: session[:sauce_type], cheese_final: session[:cheese_type], meat_final: session[:meat_type], veg_final: session[:veg_type], size_final: session[:pizza_size]}
+   erb :complete, locals: {sauce_final: session[:sauce_type], cheese_final: session[:cheese_type], meat_final: session[:meat_type], veg_final: session[:veg_type], size_final: session[:pizza_size], delivery: session[:deliver]}
   end
-#   post "/complete" do
-#     session[:sauce_type] = params[:sauce]
-#     session[:cheese_type] = params[:cheese]
-#     session[:meat_type] = params[:meat]
-#     session[:veg_type] = params[:veg]
-#     session[:pizza_size] = params[:size]
-#   end 
+  post "/complete" do
+    session[:sauce_type] = params[:sauce]
+    session[:cheese_type] = params[:cheese]
+    session[:meat_type] = params[:meat]
+    session[:veg_type] = params[:veg]
+    session[:pizza_size] = params[:size]
+    session[:deliver] = params[:delivery]
+    session[:cust_address] = params[:address]
+    redirect "/onway"
+  end 
+  get "/onway" do 
+    erb :onway, locals: {sauce_final: session[:sauce_type], cheese_final: session[:cheese_type], meat_final: session[:meat_type], veg_final: session[:veg_type], size_final: session[:pizza_size], delivery: session[:deliver], address: session[:cust_address]}
+  end
